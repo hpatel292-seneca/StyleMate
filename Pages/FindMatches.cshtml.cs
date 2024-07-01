7a610844056456b08438b0a185b9ddcc46eee9fe
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-01 17:31:58 -0400

    Add More Colour Combination and created a service for matching them

--- Pages/FindMatches.cshtml.cs
+++ Pages/FindMatches.cshtml.cs
@@ -3,30 +3,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using StyleMate.Models;
 using StyleMate.Services;
 using System.Collections.Generic;
+using System.Data;
 
 namespace StyleMate.Pages
 {
     public class FindMatchesModel : PageModel
     {
         private readonly IClothingService _clothingService;
+        private readonly IClothingCombinationService _clothingCombinationService;
 
-        public FindMatchesModel(IClothingService clothingService)
+        public FindMatchesModel(IClothingService clothingService, IClothingCombinationService clothingCombinationService)
         {
             _clothingService = clothingService;
+            _clothingCombinationService = clothingCombinationService;
         }
 
         [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
 
         public ClothingItem SelectedItem { get; set; }
-        public IEnumerable<ClothingItem> MatchingItems { get; set; }
+        public IEnumerable<ClothingCombination> MatchingItems { get; set; }
 
         public void OnGet()
         {
             SelectedItem = _clothingService.GetItemById(Id);
             if (SelectedItem != null)
             {
-                MatchingItems = _clothingService.FindMatches(SelectedItem);
+                if (SelectedItem.Type==ClothingType.Top)
+                {
+                    MatchingItems = _clothingCombinationService.GetCombinationsByTop(top: SelectedItem.Color);
+                }
+                //MatchingItems = _clothingService.FindMatches(SelectedItem);
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IClothingService, ClothingService>();
+builder.Services.AddSingleton<IClothingCombinationService, ClothingCombinationService>();
 
 var app = builder.Build();
 
