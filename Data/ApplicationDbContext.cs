1aa8801e412ed7b5b277c9554885aea30b45b090
Harshil Patel <26harshilpatel11@gmail.com>
2024-09-17 13:50:48 -0400

    Created ApplicationDBContext for utlizing Entity Framework

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StyleMate.Models;
+
+namespace StyleMate.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ClothingItem> ClothingItem { get; set; }
+        public DbSet<ClothingCombination> ClothingCombination { get; set;}
+    }
+}
