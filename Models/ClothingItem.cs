4a8698abe684bc372359e2c3d9f2ea49fab7de04
Harshil Patel <26harshilpatel11@gmail.com>
2024-09-17 13:50:14 -0400

    Update model to utilize data anntonation

--- Models/ClothingItem.cs
+++ Models/ClothingItem.cs
@@ -1,10 +1,15 @@
-﻿namespace StyleMate.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StyleMate.Models
 {
     public class ClothingItem
     {
+        [Key]
         public int Id { get; set; }
         public string? Name { get; set; }
+        [Required]
         public ClothingType Type { get; set; }
+        [Required]
         public string? Color { get; set; }
         // Add more properties as needed
     }
