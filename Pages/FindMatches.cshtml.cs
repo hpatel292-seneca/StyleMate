c61bfda5387d18885ba084e672aed339542279ba
Harshil Patel <26harshilpatel11@gmail.com>
2024-09-22 10:55:39 -0400

    Update view and view model for using GroqService

--- Pages/FindMatches.cshtml.cs
+++ Pages/FindMatches.cshtml.cs
@@ -11,9 +11,11 @@ namespace StyleMate.Pages
     {
         private readonly IClothingService _clothingService;
         private readonly IClothingCombinationService _clothingCombinationService;
+        private readonly IGroqService _groqService;
 
-        public FindMatchesModel(IClothingService clothingService, IClothingCombinationService clothingCombinationService)
+        public FindMatchesModel(IGroqService groqService, IClothingService clothingService, IClothingCombinationService clothingCombinationService)
         {
+            _groqService = groqService;
             _clothingService = clothingService;
             _clothingCombinationService = clothingCombinationService;
         }
@@ -23,25 +25,33 @@ namespace StyleMate.Pages
 
         public ClothingItem SelectedItem { get; set; }
         public IEnumerable<ClothingCombination> MatchingItems { get; set; }
+        public string LLMresponse { get; set; }
 
-        public void OnGet()
+        public async Task OnGet()
         {
             SelectedItem = _clothingService.GetItemById(Id);
+            string prompt = "";
             if (SelectedItem != null)
             {
                 if (SelectedItem.Type==ClothingType.Top)
                 {
+                    prompt = $"I have a top of {SelectedItem.Color} color and find me matches for this cloth";
                     MatchingItems = _clothingCombinationService.GetCombinationsByTop(top: SelectedItem.Color);
                 }
                 else if(SelectedItem.Type == ClothingType.Bottom){
+                    prompt = $"I have a Bottom of {SelectedItem.Color} color and find me matches for this cloth";
                     MatchingItems = _clothingCombinationService.GetCombinationsByBottom(bottom: SelectedItem.Color);
                 }
                 else if (SelectedItem.Type == ClothingType.Shoe)
                 {
+                    prompt = $"I have a Shoe of {SelectedItem.Color} color and find me matches for this cloth";
                     MatchingItems = _clothingCombinationService.GetCombinationsByShoe(shoe: SelectedItem.Color);
                 }
                 //MatchingItems = _clothingService.FindMatches(SelectedItem);
             }
+            
+            LLMresponse = await _groqService.GetChatCompletionAsync(prompt);
+            string abc;
         }
     }
 }
