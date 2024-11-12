4586659c006090c82552be2fe52ed3e548bcfcbf
Harshil Patel <26harshilpatel11@gmail.com>
2024-11-11 21:00:46 -0500

    Added views for Login and register + configure them in program.cs

--- Program.cs
+++ Program.cs
@@ -5,16 +5,28 @@ using Microsoft.Extensions.DependencyInjection;
 using StyleMate.Data;
 using StyleMate.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Use only AddDefaultIdentity to configure Identity services
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Register other services
 builder.Services.AddScoped<IClothingService, ClothingService>();
 builder.Services.AddScoped<IClothingCombinationService, ClothingCombinationService>();
 builder.Services.AddGroqClient(builder.Configuration.GetConnectionString("GROQ_API_KEY"), GroqModel.LLaMA3_8b);
 builder.Services.AddHttpClient<IGroqService, GroqService>();
 builder.Services.AddScoped<IGroqService, GroqService>();
+
+// Configure DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -24,7 +36,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -33,6 +44,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Enable authentication and authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
